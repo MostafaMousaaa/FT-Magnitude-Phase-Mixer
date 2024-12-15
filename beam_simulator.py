577b223f4bb0a9d9f2dfe973eb8e3d6749aa784d
alizayan684 <alizayan684@gmail.com>
2024-12-15 18:28:45 +0200

    Created a beam with two transmitters

--- beam_simulator.py
+++ beam_simulator.py
@@ -1,39 +1,22 @@
-import sys
+from beam_ui import Ui_MainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow
 import numpy as np
-from PyQt5.QtWidgets import QApplication, QMainWindow, \
-      QVBoxLayout, QLabel, QSlider, QPushButton, QWidget, QHBoxLayout
+from PyQt5.QtWidgets import QWidget, QVBoxLayout
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 import pyqtgraph as pg
 
-class MainWindow(QMainWindow):
+
+class MainWindow(Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Beamforming Simulator")
-        self.setGeometry(100, 100, 800, 600)
-        self.initUI()
-        self.connections()
-
-    def initUI(self):
-        layout = QVBoxLayout()
-        hlayout = QHBoxLayout()
-        self.label = QLabel("Beamforming Simulator", self)
-        layout.addWidget(self.label)
-        self.plotWidget = pg.PlotWidget(self)
-        self.slider = QSlider(self)
-        hlayout.addWidget(self.slider)
-        hlayout.addWidget(self.plotWidget)
-        layout.addLayout(hlayout)        
-        self.button = QPushButton("Simulate", self)
-        layout.addWidget(self.button)
-        container = QWidget()
-        container.setLayout(layout)
-        self.setCentralWidget(container)
-
-    def connections(self):
-        self.button.clicked.connect(self.beamform)
-
-    def beamform(self): 
-        x = np.linspace(0, 10, 1000) 
-        y = np.linspace(0, 10, 1000) 
+        self.setupUi(self)
+        self.formUniformWaterWave()
+    
+    def formUniformWaterWave(self):
+        """this functon forms the uniform water wave that transmits through the beam from a point source"""
+        x = np.linspace(-10, 10, 1000) 
+        y = np.linspace(-10, 10, 1000) 
         X, Y = np.meshgrid(x, y) 
         wavelength = 2
         d = wavelength / 4 
@@ -42,16 +25,11 @@ class MainWindow(QMainWindow):
         frequency = 1 
         Z1 = np.sin(2 * np.pi * frequency * R1 / wavelength) 
         Z2 = np.sin(2 * np.pi * frequency * R2 / wavelength) 
-        Z = Z1 + Z2  
-        self.plotWidget.clear() 
-        img = pg.ImageItem(Z) 
-        self.plotWidget.addItem(img)
-        emitters = np.array([[-d, 0], [d, 0]]) 
-        scatter = pg.ScatterPlotItem(pos=emitters, pen=pg.mkPen(None), brush=pg.mkBrush('r'), size=10) 
-        self.plotWidget.addItem(scatter)
-
+        Z = Z1 + Z2
+        self.WavesGraph.plot_wave(Z)
 if __name__ == "__main__":
+    import sys
     app = QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
+    mainWindow = MainWindow()
+    mainWindow.show()
     sys.exit(app.exec_())
